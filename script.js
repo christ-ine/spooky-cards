4063df1243478858a884061f8a68dd0c5073473e
Christine Ross <christinejoyross@gmail.com>
2020-10-21 19:50:04 -0700

    matching functions

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ class AudioController {
         this.flipSound = new Audio("Assets/Audio/flip.mp3");
         this.matchSound = new Audio("Assets/Audio/match.wav");
         this.victorySound = new Audio("Assets/Audio/victory.mp3");
-        this.gameOverSound = new Audio("Assets/Audio/gameover.mp3");
+        this.gameOverSound = new Audio("Assets/Audio/gamover.mp3");
         this.bgMusic.volume = 0.5;
         this.bgMusic.loop = true;
     }   
@@ -15,7 +15,7 @@ class AudioController {
 
     stopMusic(){
         this.bgMusic.pause();
-        this.bgMusic.currentTime = 0;
+        this.bgMusic.currentTime = 5;
     }
 
     flip(){
@@ -42,7 +42,7 @@ class MixOrMatch {
         this.cardsArray = cards;
         this.totalTime = totalTime;
         this.timeRemaining = totalTime;
-        this.timer = document.getElementById('time-remainer');
+        this.timer = document.getElementById('time-remaining');
         this.ticker = document.getElementById('flips');
         this.AudioController = new AudioController();
     }
@@ -64,6 +64,103 @@ class MixOrMatch {
         this.ticker.innerText = this.totalClicks;
     }
 
+    startCountdown(){
+        return setInterval(() => {
+            this.timeRemaining--;
+            this.timer.innerText = this.timeRemaining;
+            if(this.timeRemaining === 0){
+                this.gameOver();
+            }
+
+        }, 1000);
+    }
+
+    gameOver(){
+        clearInterval(this.countdown);
+        this.AudioController.gameOver();
+        document.getElementById('game-over-text').classList.add('visible');
+    }
+
+    victory(){
+        clearInterval(this.countdown);
+        this.AudioController.victory();
+        document.getElementById('victory-text').classList.add('visible')
+    }
+
+    hideCards(){
+        this.cardsArray.forEach(card => {
+            card.classList.remove('visible');
+            card.classList.remove('matched');
+        })
+    }
+
+    flipCard(card){
+        if(this.canFlipCard(card)){
+            this.AudioController.flip();
+            this.totalClicks++;
+            this.ticker.innerText = this.totalClicks;
+            card.classList.add('visible');
+
+            if(this.cardToCheck){
+                this.checkForCardMatch(card)
+            } else {
+                this.cardToCheck = card;
+            }
+        }
+    }
+
+    checkForCardMatch(card){
+        if(this.getCardType(card) === this.getCardType(this.cardToCheck)){
+            this.cardMatch(card, this.cardToCheck)
+            console.log("match!")
+        } else {
+            this.cardMismatch(card, this.cardToCheck)
+            console.log("no match")
+        }
+        this.cardToCheck = null;
+    }
+
+    cardMatch(card1, card2){
+        this.matchedCards.push(card1);
+        this.matchedCards.push(card2);
+        card1.classList.add('matched');
+        card2.classList.add('matched');
+        console.log(this.matchedCards);
+        console.log(this.cardsArray);
+        this.AudioController.match();
+
+        if(this.matchedCards.length === this.cardsArray.length) {
+            this.victory()
+        }
+
+    }
+
+    cardMismatch(card1, card2){
+        this.busy = true;
+        setTimeout(() => {
+            card1.classList.remove('visible');
+            card2.classList.remove('visible');
+            this.busy = false;
+        }, 1000)
+    }
+
+    // fisher yates shuffle algortithim
+    shuffleCards(cardsArray){
+        for(let i = cardsArray.length -1; i>0; i--){
+            let randIndex = Math.floor(Math.random() * (i + 1));
+            cardsArray[randIndex].style.order = 1
+            cardsArray[i].style.order = randIndex;
+        }
+    }
+
+    getCardType(card){
+        return card.getElementsByClassName('card-value')[0].src;
+    }
+
+    canFlipCard(card){
+        return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck;
+    }
+
     
 }
 
@@ -80,11 +177,12 @@ if(document.readyState === 'loading'){
 function ready (){
     let overlays = Array.from(document.getElementsByClassName('overlay-text'));
     let cards = Array.from(document.getElementsByClassName('card'));
+    let game = new MixOrMatch(100, cards)
 
     overlays.forEach(overlay => {
         overlay.addEventListener('click', () => {
             overlay.classList.remove('visible');
-            //game.startGame();
+            game.startGame();
         })
     })
 
